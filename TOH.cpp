59e2456bff494ae8dc7eb4997cde87c138d63c32
h4-harshh <harshnew2204@gmail.com>
2023-01-12 23:20:13 +0530

    tower of hanoi

--- TOH.cpp
+++ TOH.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
+
+void TOH(int n,char A,char B,char C)
+{
+    if(n==0)
+    {
+        return;
+    }
+    TOH(n-1,A,C,B);
+    cout<<"Move disc "<<n<<" from "<<A<<" to "<<C<<endl;
+    TOH(n-1,B,A,C); 
+}
+
 int main()
 {
-    
+    int n;
+    cout<<"Enter the number of discs"<<endl;
+    cin>>n;
+    TOH(n,'A','B','C');
     return 0;
 }
