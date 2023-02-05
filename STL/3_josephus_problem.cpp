69fce180f31d5fe307c521f363fdcce3e6880008
h4-harshh <harshnew2204@gmail.com>
2023-02-05 22:05:41 +0530

    josephus problem using stl list

---
+++ STL/3_josephus_problem.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int get_item(int n,int k)
+{
+    list <int> l;
+
+    for(int i=0;i<n;i++)
+    {
+        l.push_back(i);
+    }
+
+    auto it=l.begin();
+
+    while(l.size()>1)
+    {
+        for(int count=1;count<k;count++)
+        {
+            it++;
+            if(it==l.end())
+                it=l.begin();
+        }
+        it=l.erase(it);
+        if(it==l.end())
+            it=l.begin();
+    }
+
+    return *l.begin();
+}
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+
+    int h=get_item(n,k);
+
+    
+    cout<<h;
+
+    return 0;
+}
