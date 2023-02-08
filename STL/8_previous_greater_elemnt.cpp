771a2e0482989c6d8d6c6eb3c380b7c61942c379
h4-harshh <harshnew2204@gmail.com>
2023-02-08 14:46:52 +0530

    previous greater

--- STL/8_previous_greater_elemnt.cpp
+++ STL/8_previous_greater_elemnt.cpp
@@ -1,41 +1,75 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// // vector<int>
+// void span(int arr[],int n)
+// {
+//     // vector <int> v;
+//     for(int i=0;i<n;i++)
+//     {
+//         int span=0;
+//         for(int j=i-1;j>=0;j--)
+//         {
+//             if(arr[j]>arr[i])
+//             {
+//                 // v.push_back(arr[j]);
+//                 cout<<arr[j]<<" ";
+//                 span=1;
+//                 break;
+//             }
+//         }
+//         if(span!=1)
+//         {
+//             cout<<"-1"<<" ";
+//         }
+      
+//     }
+// }
+
+
+// int main()
+// {
+//     int n=7;
+//     int arr[n]={15,10,18,12,4,6,28};
+//     // vector <int> s=
+//     span(arr,n);
+
+//     // for(int x:s)
+//     //     cout<<x<<" ";
+//     // return 0;
+// }
+
+// -----------------------------------------------
+// using stock span problem;
+
+
 #include<iostream>
-#include<vector>
+#include<stack>
 using namespace std;
 
-// vector<int>
-void span(int arr[],int n)
+void previous_greater(int arr[],int n)
 {
-    // vector <int> v;
-    for(int i=0;i<n;i++)
+    stack <int> s;
+    s.push(arr[0]);
+    cout<<"-1"<<" ";
+    for(int i=1;i<n;i++)
     {
-        int span=0;
-        for(int j=i-1;j>=0;j--)
+        while(s.empty()==false && s.top()<arr[i])
         {
-            if(arr[j]>arr[i])
-            {
-                // v.push_back(arr[j]);
-                cout<<arr[j]<<" ";
-                span=1;
-                break;
-            }
+            s.pop();
         }
-        if(span!=1)
-        {
-            cout<<"-1"<<" ";
-        }
-      
+
+        int k=((s.empty())?-1:s.top());
+        cout<<k<<" ";
+        s.push(arr[i]);
     }
 }
-
-
 int main()
 {
     int n=7;
     int arr[n]={15,10,18,12,4,6,28};
-    // vector <int> s=
-    span(arr,n);
+    previous_greater(arr,n);
 
-    // for(int x:s)
-    //     cout<<x<<" ";
-    // return 0;
+    return 0;
 }
