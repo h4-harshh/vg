e04a7fde8a7dbf63b49f0fda39448736a1afda87
h4-harshh <harshnew2204@gmail.com>
2023-02-13 19:00:38 +0530

    thief problem in cpp stl

---
+++ STL/31_thief_problem.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int max(int arr[],int n,int k)
+{
+    sort(arr,arr+n);
+
+    // sort(arr,arr+n,greater<int>());
+    int sum=0;
+    for(int i=n-1;i>=n-k;i--)
+    {
+        sum=sum+arr[i];
+    }
+
+    return sum;
+}
+
+
+
+int main()
+{
+    int n=6;
+    int k=3;
+    int arr[6]={3,7,2,5,12,30};
+
+    cout<<max(arr,n,k);
+
+
+
+    return 0;
+}
